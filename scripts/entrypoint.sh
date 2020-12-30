506fe958b43049572d4cc77d17fb1046bdca925c
Pascal Karg <dev@pascalkarg.de>
2020-12-30 19:14:06 +0100

    Update entrypoint.sh to copy files to the main dir

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-cd /minecraft/data
 
 # Check if directory exists and if files are present
-if ! ls -1qA $DIR | grep -q . ; then
-        echo "Copying files from /minecraft/conf/ to /minecraft/data"
-	# Copy all not already existing config files to the data folder
+if ls -1qA  /minecraft/data | grep -q . ; then
+        echo "Copying files from /minecraft/conf/ to /minecraft/conf"
+        # Copy all not already existing config files to the main folder
         cp -n /minecraft/conf/* .
+else
+        echo "Skip copying files..."
 fi
 
 echo "eula=$EULA" >> eula.txt
 java -jar -Xmx$MAX_MEMORY -Xms$MIN_MEMORY -jar /minecraft/server.jar nogui
+~                                                                                                                                                                                                                                                     
+~                                                                                                                                                                                                                                                     
+~                                                                               
