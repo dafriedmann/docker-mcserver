51e5882b460fa4d5474bfb4567ef705a37438d1d
Pascal Karg <dev@pascalkarg.de>
2020-12-30 19:27:58 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,13 +4,12 @@
 if ls -1qA  /minecraft/data | grep -q . ; then
         echo "Copying files from /minecraft/conf/ to /minecraft/conf"
         # Copy all not already existing config files to the main folder
-        cp -n /minecraft/conf/* .
+        cp -n /minecraft/conf/* /minecraft/data
 else
         echo "Skip copying files..."
 fi
 
+cd /minecraft/data
 echo "eula=$EULA" >> eula.txt
 java -jar -Xmx$MAX_MEMORY -Xms$MIN_MEMORY -jar /minecraft/server.jar nogui
-~                                                                                                                                                                                                                                                     
-~                                                                                                                                                                                                                                                     
-~                                                                               
+
